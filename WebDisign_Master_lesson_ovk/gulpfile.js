865a2de39f4adb57977a51b47cf2fbca786a0ca5
GavrikGal <gavrik.gal@gmail.com>
2016-08-22 17:57:17 +0300

    Lesson_ovk, стартовый шаблон
    Подготовка стартового шаблона для начала прохождения уроков

---
+++ WebDisign_Master_lesson_ovk/gulpfile.js
@@ -0,0 +1,138 @@
+var gulp               = require('gulp'),
+		compass        = require('gulp-compass'),
+		path           = require('path'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		pngquant       = require('imagemin-pngquant'),
+		cache          = require('gulp-cache'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		fileinclude    = require('gulp-file-include'),
+		gulpRemoveHtml = require('gulp-remove-html'),
+		bourbon        = require('node-bourbon'),
+		notify         = require("gulp-notify"),
+		ftp            = require('vinyl-ftp');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('compass', ['headercompass'], function() {
+	return gulp.src(['app/scss/**/*.scss','!app/scss/libs.scss'])
+		.pipe(compass({
+				project: path.join(__dirname, 'app'),
+				// css: 'css',
+				sass: 'scss',
+				fonts_dir : 'fonts',
+				})).on("error", notify.onError('Something is Wrong in COMPASS task!'))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('headercompass', function() {
+	return gulp.src('app/header.scss')
+		.pipe(compass({
+				// project: path.join(__dirname, 'app'),
+				css: 'app/css',
+				sass: 'app',
+				fonts_dir : 'app/fonts',
+				})).on("error", notify.onError('Something is Wrong in COMPASS task!'))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('libs', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		// 'app/libs/magnific-popup/magnific-popup.min.js'
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('watch', ['compass', 'libs', 'browser-sync'], function() {
+	gulp.watch('app/header.scss', ['headercompass']);
+	gulp.watch('app/scss/**/*.scss', ['compass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('buildhtml', function() {
+  gulp.src(['app/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@'
+    }))
+    .pipe(gulpRemoveHtml())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+
+gulp.task('build', ['removedist', 'buildhtml', 'imagemin', 'compass', 'libs'], function() {
+
+	var buildCss = gulp.src([
+		'app/css/fonts.min.css',
+		'app/css/main.min.css'
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildFiles = gulp.src([
+		'app/.htaccess'
+	]).pipe(gulp.dest('dist'));
+
+	var buildFonts = gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*').pipe(gulp.dest('dist/js'));
+
+});
+
+gulp.task('deploy', ['build'], function() {
+
+	var conn = ftp.create({
+		host:      'hostname.com',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+	'dist/**',
+	'dist/.htaccess',
+	];
+	return gulp.src( globs, { buffer: false } )
+	.pipe( conn.dest( '/path/to/folder/on/server' ) );
+
+});
+
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
